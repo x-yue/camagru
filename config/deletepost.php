3c74a0800ab4a73d38ca538bb750c5a2ec45b6f6
yuxu <yuxu@github.com>
2019-03-22 20:07:56 +0100

    delete post work with both feed and home:

--- config/deletepost.php
+++ config/deletepost.php
@@ -2,6 +2,12 @@
 
 include "setup.php";
 
+function error(){
+    echo "<script>alert('Something went wrong, please try again.')</script>";
+    echo "<script>location.href = '../mygallery.php';</script>";
+	exit;
+}
+
 session_start();
 if (!isset($_SESSION['username'])){  
     echo "<script>alert('You need to sign in first.')</script>";
@@ -10,13 +16,18 @@ if (!isset($_SESSION['username'])){
     $name = $_SESSION["username"];
 }
 
-$creation_time = $_POST["time"];
-$imgloc = $_POST["imgloc"];
-
-$conn = db_connect();
-$sql_time = "DELETE FROM imagelist WHERE username = '$name' AND image_location = '$imgloc' AND date_creation = '$creation_time'";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$conn = null;
-
+if ($_POST['delete'] == "Delete" && $_POST["createtime"] && $_POST["imgname"] && $_POST["username"])
+{
+    $createtime = $_POST["createtime"];
+    $imgname = $_POST["imgname"];
+    $username = $_POST["username"];
+    $conn = db_connect();
+    $sql = "DELETE FROM imagelist WHERE username = '$username' AND image_location = '$imgname' AND date_creation = '$createtime'";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $conn = null;
+    echo "<script>location.href='../mygallery.php'</script>";
+} else {
+    error();
+}
 ?>
